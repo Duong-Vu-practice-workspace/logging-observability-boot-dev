38ee0d444c311def9ff6b5f85fe6691b50b7b93b
Duong Vu Cong Tuan <vucongtuanduong2409@gmail.com>
2026-08-04 13:41:57 +0700

    Integrating pprof

--- server.go
+++ server.go
@@ -9,6 +9,7 @@ import (
 	"log/slog"
 	"net"
 	"net/http"
+	"net/http/pprof"
 	"os"
 	"time"
 
@@ -136,6 +137,8 @@ func newServer(store store.Store, port int, cancel context.CancelFunc, logger *s
 
 	mux.HandleFunc("GET /", s.handlerIndex)
 	mux.Handle("GET /metrics", promhttp.Handler())
+	mux.HandleFunc("GET /debug/pprof/", pprof.Index)
+	mux.HandleFunc("GET /debug/pprof/profile", pprof.Profile)
 	mux.Handle("POST /api/login", s.authMiddleware(http.HandlerFunc(s.handlerLogin)))
 	mux.Handle("POST /api/shorten", s.authMiddleware(http.HandlerFunc(s.handlerShortenLink)))
 	mux.Handle("GET /api/stats", s.authMiddleware(http.HandlerFunc(s.handlerStats)))
